55006a52dbde292915380c5331bfd64ed7a9666d
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-04-10 10:05:50 -0400

    Easy BOOOOLLL

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -1,17 +1,37 @@
-//
-//  main.swift
-//  RotatingLetters
-//
-//  Created by Gordon, Russell on 2018-04-06.
-//  Copyright © 2018 Gordon, Russell. All rights reserved.
-//
 
 import Foundation
 
 // Get the user input
 var rawInput = readLine()
+guard let input = rawInput else {
+    exit(9)
+}
 
 // Print out the input provided
 print("You said:")
 print(rawInput)
+
+//func inputAccepter (_ input: String) -> Bool {
+//    var inputVariable = input
+//    if inputVariable.contains {
+//
+//    }
+//    return true
+//}
+var acceptableCharacters : Bool = true
+for individualCharacters in input {
+    switch individualCharacters {
+    case "I", "O", "H", "Z", "N", "S" :
+        acceptableCharacters = true
+    
+    default :
+        acceptableCharacters = false
+        break
+    }
+}
+if acceptableCharacters == true {
+    print("Yes.The sun is shining")
+} else if acceptableCharacters == false {
+    print("Today the God isn't as benevolent as it was in the era of The Bible so your output is cow.")
+}
 
